baa1103436650fe28ce2896c70f8db1da9a867ed
Chelsea Carr <carrche2@msu.edu>
2014-03-02 13:29:02 -0800

    Committing base files

---
+++ nearbyLocations/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  nearbyLocations
+//
+//  Created by Chelsea Carr on 3/2/14.
+//  Copyright (c) 2014 Chelsea Carr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
