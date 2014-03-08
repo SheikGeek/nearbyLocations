3b84f54c79332f02bd9ca843ce2142c1db1b1dc7
Chelsea Carr <carrche2@msu.edu>
2014-03-08 01:01:55 -0800

    Adding initial project files

--- nearbyLocations/nearbyLocations/AppDelegate.h
+++ nearbyLocations/nearbyLocations/AppDelegate.h
@@ -2,7 +2,7 @@
 //  AppDelegate.h
 //  nearbyLocations
 //
-//  Created by Chelsea Carr on 3/2/14.
+//  Created by Chelsea Carr on 3/8/14.
 //  Copyright (c) 2014 Chelsea Carr. All rights reserved.
 //
 
